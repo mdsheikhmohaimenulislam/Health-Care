13561ea173cf33da5decf914f6e589426aceda00
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-09-12 08:22:41 +0600

    feat:Login to your account  section created successfully

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import Providers from "@/providers";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,11 +25,17 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+      className={cn(
+        "h-full",
+        "antialiased",
+        geistSans.variable,
+        geistMono.variable,
+        "font-sans",
+        inter.variable,
+      )}
     >
       <Providers>
-      <body className="min-h-full flex flex-col">{children}</body>
-
+        <body className="min-h-full flex flex-col">{children}</body>
       </Providers>
     </html>
   );
