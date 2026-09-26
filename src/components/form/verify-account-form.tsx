b321042e99d2f9af199683819d8c76062871d2c6
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-09-26 23:00:31 +0600

    feat:just comment

--- src/components/form/verify-account-form.tsx
+++ src/components/form/verify-account-form.tsx
@@ -20,7 +20,7 @@ import { useEffect, useState } from "react";
 
 const RESEND_COOLDOWN = 120;
 
-// =================....,,,,,,
+// =================....,sss,,,,,
 
 export default function VerifyAccountForm() {
   const searchParams = useSearchParams();
